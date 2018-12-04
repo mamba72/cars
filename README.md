530c224b9bb70bbc7e6a61f370be3337bd5ca40b
mamba72 <saltybanana72@gmail.com>
2018-12-04 15:20:11 -0800

    Adds cars.sh

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # cars
+Author: Stephen White
+This program is supposed to store car years, makes, models into a text file.
+Then you can add a car
+Read them back sorted by year
+or quit.
--- cars.sh
+++ cars.sh
@@ -9,7 +9,8 @@ do
 	echo "1. Add Car, 2. List Cars, 3. Quit"
 	read choice
 
-	case $choice in "1")
+	case $choice in 
+	"1")
 		echo "What is the year of the car?"
 		read carYear
 		echo "Who makes the car?"
@@ -18,15 +19,14 @@ do
 		read carModel
 		carName=$carYear":"$carMake":"$carModel
 		echo $carName >> "My_old_cars.txt"
-
-	case $choice in "2")
-		while read -r line; do
-			echo $line
-
-		done < "My_old_cars.txt"
-		
+		;;
+	"2")
+		sort -n "My_old_cars.txt"
+		;;
+	"3")
+		stay=false
+		;;	
 	esac
-stay=false
 done
-
-
+echo "Thanks for your time!"
+echo "Have a good day!"
